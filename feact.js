ad0fd0e26b6b062af060072746562a2effcd4c25
MBearo <wuweiqi@baidu.com>
2022-01-13 15:46:44 +0800

    fiber

--- feact.js
+++ feact.js
@@ -19,20 +19,77 @@ export function createElement (type, props, ...children) {
         }
     }
 }
+let nextUnitOfWork = null
 export function render (element, container) {
-    // container.appendChild(element)
-    const dom = element.type === 'TEXT_ELEMENT'
+    nextUnitOfWork = {
+        dom: container,
+        props: {
+            children: [element]
+        }
+    }
+    while (nextUnitOfWork) {
+        nextUnitOfWork = performUnitOfWork(
+            nextUnitOfWork
+        )
+    }
+}
+
+// 执行当前的子任务，返回下一个子任务
+function performUnitOfWork (fiber) {
+    // 将dom添加到fiber中
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
+    }
+    // 这没懂
+    if (fiber.parent) {
+        fiber.parent.dom.appendChild(fiber.dom)
+    }
+    // 为子元素创建fiber节点
+    const elements = fiber.props.children
+    let index = 0
+    let prevSibling = null
+    while (index < elements.length) {
+        const element = elements[index]
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null
+        }
+        if (index === 0) {
+            fiber.child = newFiber
+        } else {
+            // 实现添加同级节点，sibling是下一个兄弟节点
+            // 实现的很巧妙，类似vue的那个watch绑定
+            prevSibling.sibling = newFiber
+        }
+        prevSibling = newFiber
+        index++
+    }
+    // 返回下一个子任务
+    // 顺序是子节点、兄弟节点、叔叔节点
+    if (fiber.child) {
+        return fiber.child
+    }
+    let nextFiber = fiber
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.parent
+    }
+}
+
+function createDom (fiber) {
+    const dom = fiber.type === 'TEXT_ELEMENT'
         ? document.createTextNode('')
-        : document.createElement(element.type)
+        : document.createElement(fiber.type)
 
     const isProperty = key => key !== 'children'
-    Object.keys(element.props)
+    Object.keys(fiber.props)
         .filter(isProperty)
         .forEach(name => {
-            dom[name] = element.props[name]
+            dom[name] = fiber.props[name]
         })
-    element.props.children.forEach(child => {
-        render(child, dom)
-    })
-    container.appendChild(dom)
+    return dom
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import { createElement, render } from '../feact'
 
+const aaaa = []
+for (let i = 0; i < 10; i++) {
+    aaaa.push(<div>{i}</div>)
+}
+console.log('aaaa', aaaa)
 const f = (
     <div id='a'>
         <span>fff</span>
         <span>bbb</span>
         jjj
         <div>ssss</div>
+        <div>1</div>
     </div>
 )
 
